fc69e6266ad71219f51481b2e75aa9bf12601792
MlynorikAlina <alina.sudnick@gmail.com>
2021-03-28 23:52:37 +0300

    28.03

--- html+css/posts.js
+++ html+css/posts.js
@@ -1,71 +1,80 @@
 
-let dateParser = ""
-function makePost(id,user,time,text){
-		return{
-			id,
-			user,
-			time,
-			text
-		};
-}
-
-let myPosts =  new class postProcessing{
-	#posts = [];
-	#postsList = document.getElementById("posts-list");
+class postProcessing{
 
 	constructor(postsToAdd){
-		this.#posts = postsToAdd;
+		this._posts = postsToAdd;
 	}
 	loadPosts(){
-		this.#posts.push(makePost("1","@User","11.11.11 2:15","some data"));
-		this.#posts.push(makePost("2","@User","11.11.11 2:15","some data"));
-		this.posts.push(makePost("3","@User","11.11.11 2:15","some data"));
-		this.posts.push(makePost("4","@User","11.11.11 2:15","some data"));
-		this.posts.push(makePost("5","@User","11.11.11 2:15","some data"));
-		this.posts.push(makePost("6","@User","11.11.11 2:15","some data"));
-		this.posts.push(makePost("7","@User","11.11.11 2:15","some data"));
-		this.posts.push(makePost("8","@User","11.11.11 2:15","some data"));
-		this.posts.push(makePost("9","@User","11.11.11 2:15","some data"));
-		this.posts.push(makePost("10","@User","11.11.11 2:15","some data"));
-		this.posts.push(makePost("11","@User","11.11.11 2:15","some data"));
-		this.posts.push(makePost("12","@User","11.11.11 2:15","some data"));
-		this.posts.push(makePost("13","@User","11.11.11 2:15","some data"));
-		this.posts.push(makePost("14","@User","11.11.11 2:15","some data"));
-		this.posts.push(makePost("15","@User","11.11.11 2:15","some data"));
-		this.posts.push(makePost("16","@User","11.11.11 2:15","some data"));
-		this.posts.push(makePost("17","@User","11.11.11 2:15","some data"));
-		this.posts.push(makePost("18","@User","11.11.11 2:15","some data"));
-		this.posts.push(makePost("19","@User","11.11.11 2:15","some data"));
-		this.posts.push(makePost("20","@User","11.11.11 2:15","some data"));
+		this._posts.push(this.#makePost("1","@User1","2011.01.11 02:15","KLD","7584"));
+		this._posts.push(this.#makePost("1","@User","2011.11.11 02:15","KLD","7584"));
+		this._posts.push(this.#makePost("1","@User","2011.11.11 02:15","KLD","7584"));
+		this._posts.push(this.#makePost("1","@User","2011.11.11 02:15","KLD","7584"));
+		this._posts.push(this.#makePost("1","@User","2011.11.11 02:15","KLD","7584"));
+		this._posts.push(this.#makePost("1","@User","2011.11.11 02:15","KLD","7584"));
+		this._posts.push(this.#makePost("1","@User","2011.11.11 02:15","KLD","7584"));
+		this._posts.push(this.#makePost("1","@User","2011.11.11 02:15","KLD","7584"));
+		this._posts.push(this.#makePost("1","@User","2011.11.11 02:15","KLD","7584"));
+		this._posts.push(this.#makePost("1","@User","2011.11.11 02:15","KLD","7584"));
+		this._posts.push(this.#makePost("1","@User","2011.11.11 02:15","KLD","7584"));
+		this._posts.push(this.#makePost("1","@User","2011.11.11 02:15","KLD","7584"));
+		this._posts.push(this.#makePost("1","@User","2011.11.11 02:15","KLD","7584"));
+		this._posts.push(this.#makePost("1","@User","2011.11.11 02:15","KLD","7584"));
+		this._posts.push(this.#makePost("1","@User","2011.11.11 02:15","KLD","7584"));
+		this._posts.push(this.#makePost("1","@User","2011.11.11 02:15","KLD","7584"));
+		this._posts.push(this.#makePost("1","@User","2011.11.11 02:15","KLD","7584"));
+		this._posts.push(this.#makePost("1","@User1","2011.11.11 02:15","KLD","7584"));
+		this._posts.push(this.#makePost("1","@User","2011.11.11 02:15","KLD","7584"));
+		this._posts.push(this.#makePost("1","@User5","2001.11.15 02:15","KLD","7584"));
 	}
 
+	print(){
+		console.log(this._posts);
+	}
+	#makePost(id,user,time,company,taxiNum){
+	if(typeof user == "string" && RegExp("^@[0-9a-zA-z]+$").test(user))
+			if(typeof time == "string" && RegExp("^[0-9]{4}\.[0-9]{2}\.[0-9]{2}\\s[0-9]{2}:[0-9]{2}$").test(time))
+				if(typeof id == "string" && RegExp("^[0-9]+$").test(id))
+					if(typeof company == "string" && typeof taxiNum == "string"){
+	                    let date = new Date(Date.parse(time.replace( /(\d{4})\.(\d{2})\.(\d{4})/, '$3-$2-$1')));
+	                    return{
+	                    	id,
+	                    	user,
+	                    	date,
+	                    	company,
+	                    	taxiNum
+	                    };
+		
+	}
+    }
+
 	getPostById(id){
-		return posts.find((el,elId,arr)=>{return el.id == id;});
+		return this._posts.find((el,elId,arr)=>{return el.id == id;});
 	}
 	validatePost(Post){
-		if(typeof Post.user == "string" && new RegExp("^@[0-9a-zA-z]+$").test(Post.user))
-			if(typeof Post.time == "string" && new RegExp("^[0-9]{2}\.[0-9]{2}\.[0-9]{2}\s[0-9]{2}:[0-9]{2}$").test(Post.time))
-				if(typeof Post.id == "string" && new RegExp("^[0-9]+$").test(Post.id))
-					if(typeof Post.text == "string") return true;
+		if(typeof Post.user == "string" && RegExp("^@[0-9a-zA-z]+$").test(Post.user))
+			if(typeof Post.time == Date)
+				if(typeof Post.id == "string" && RegExp("^[0-9]+$").test(Post.id))
+					if(typeof Post.company == "string"&&typeof Post.taxiNum == "string") return true;
 		return false;
 	}
 	addPost(Post) {
-		if(validatePost(Post))this.posts.push(Post);
+		if(validatePost(Post))this._posts.push(Post);
 		else alert("Error::addPost");
 	}
 	addAll(postsToAdd){
-		postsToAdd.forEach(el=>{if(validatePost(el))this.posts.push(el);});
+		postsToAdd.forEach((el)=>{if(this.validatePost(el))this._posts.push(el);});
 	}
-	getPosts(skip = 0, top = 10, filterConfig){
-		return posts.filter(el=>filterConfig.user == el.user && filterConfig.timeFrom <= el.time && filterConfig.timeTo >= el.time).slice(skip,skip+top);
+	getPosts(skip = 0, top = 10, filterConfig = {user:null,timeFrom:null,timeTo:null}){
+		return this._posts.filter(el=>(filterConfig.user == null || filterConfig.user == el.user) 
+			&& (filterConfig.timeFrom == null|| filterConfig.timeFrom<= el.time) && (filterConfig.timeTo == null ||filterConfig.timeTo >= el.time)).slice(skip,skip+top);
 	}
-	print(){
-		console.log(this.posts);
+	getPostsNum(){
+		return this._posts.length;
 	}
+	
 }
 
-
-function command(cmnd)  {
+function command(cmnd,id=1)  {
     switch(cmnd)  {
         case "loadPosts":
             myPosts.loadPosts();
@@ -73,24 +82,35 @@ function command(cmnd)  {
         case "printPosts":
             myPosts.print();
             break;
+        case "getPostById":
+            console.log(myPosts.getPostById(id));
+            break;
+        case "validatePost":
+            console.log(myPosts.validatePost(myPosts.getPostById(id)));
+            break;
+
     }
 }
 
-
-/*function CreateNewPost(username,time,company,taxiNum){
+function CreateNewPost(Post){
    let li = document.createElement('li');
+   options = {
+   year: '2-digit', month: '2-digit', day: '2-digit',
+   hour: '2-digit', minute: '2-digit',
+   hour12: false
+   };
    li.innerHTML = `<div class="post font-Marcellius">
 							<img id="User-img" src="img/user.png">
-						    <p class="User content">${username}</p>
-						    <p class="Time content">${time}</p>
+						    <p class="User content">${Post.user}</p>
+						    <p class="Time content">${new Intl.DateTimeFormat('en-GB',options).format(Post.date)}</p>
 						<div class="main-context">
 						<div>
 							<p class="content">Taxi company:</p>
-							<p class="content">${company}</p>
+							<p class="content">${Post.company}</p>
 						</div>
 						<div>
 							<p class="content">Taxi number:</p>
-						    <p class="content">${taxiNum}</p>
+						    <p class="content">${Post.taxiNum}</p>
 						</div>\
 						<div>\
 							<img height="30px" src="img/like.png">\
@@ -101,5 +121,76 @@ function command(cmnd)  {
 					</div>`
 	return li;
 }
-var postsList = document.getElementById('posts-list');
-postsList.appendChild(CreateNewPost("@Marvellson","11.11.11 11:25","Pic taxi","15486"));*/
+
+
+let myPosts =  new postProcessing(new Array());
+let postsOnPage = 0;
+let filterConfig = {user:null,timeFrom:null,timeTo:null};
+let tempFilterConfig = {user:null,timeFrom:null,timeTo:null};
+let maxPostsCount = 0;
+
+myPosts.loadPosts();
+
+document.addEventListener("DOMContentLoaded",function(){
+	let postsList = document.getElementById('posts-list');
+	maxPostsCount = myPosts.getPostsNum();
+    myPosts.getPosts().forEach((el)=>{postsList.appendChild(CreateNewPost(el))});
+    postsOnPage += 10;
+});
+
+function showMore(button){
+	let postsList = document.getElementById('posts-list');
+	myPosts.getPosts(postsOnPage,10,filterConfig).forEach((el)=>{postsList.appendChild(CreateNewPost(el))});
+	postsOnPage += 10;
+	if(postsOnPage >= maxPostsCount){
+		button.style.display = "none";
+	}
+}
+function filterByUser(inputUser){
+	if(typeof inputUser.value == "string" && RegExp("^@[0-9a-zA-z]+$").test(inputUser.value)){
+		inputUser.style.color = '#848484';
+		tempFilterConfig.user = inputUser.value;
+	}else{
+		inputUser.style.color = '#FF0000';
+	}
+}
+function filterByTimeFrom(inputTimeFrom){
+	if(typeof inputTimeFrom.value == "string" && RegExp("^[0-9]{4}\.[0-9]{2}\.[0-9]{2}\\s[0-9]{2}:[0-9]{2}$").test(inputTimeFrom.value)){
+		inputTimeFrom.style.color = '#848484';
+		tempFilterConfig.timeFrom = inputTimeFrom.value;
+	}else{
+		inputTimeFrom.style.color = '#FF0000';
+	}
+}
+function filterByTimeTo(inputTimeTo){
+	if(typeof inputTimeTo.value == "string" && RegExp("^[0-9]{4}\.[0-9]{2}\.[0-9]{2}\\s[0-9]{2}:[0-9]{2}$").test(inputTimeTo.value)){
+		inputTimeTo.style.color = '#848484';
+		tempFilterConfig.timeTo = inputTimeTo.value;
+	}else{
+		inputTimeTo.style.color = '#FF0000';
+	}
+}
+function filterPosts(){
+	filterConfig = tempFilterConfig;
+
+	postsList = document.getElementById('posts-list');
+	showMoreButton = document.getElementById('show-more');
+	showMoreButton.style.display = 'inline-block';
+    while (postsList.firstChild) {
+    postsList.removeChild(postsList.firstChild);
+    }
+    postsOnPage = 0;
+    let posts = myPosts.getPosts(postsOnPage,myPosts.getPostsNum(),filterConfig);
+    maxPostsCount = posts.length;
+    if(posts.length != 0){
+    	posts.slice(postsOnPage,10).forEach((el)=>{postsList.appendChild(CreateNewPost(el))});
+    	postsOnPage += 10;
+    }else{
+    	 let li = document.createElement('li');
+    	 li.innerHTML='No results';
+    	 postsList.appendChild(li);
+    }
+    if(postsOnPage >= maxPostsCount){
+		showMoreButton.style.display = "none";
+	}
+}
